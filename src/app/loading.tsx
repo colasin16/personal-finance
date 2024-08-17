5628b278a4ab2f02f40218b8a28894cc57b3cb39
Jordi Colas <jordi.colas@viaplaygroup.com>
2024-08-17 15:25:15 +0200

    chore(project): Copied Minimal UI over to this project

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { SplashScreen } from '@src/app/_components/loading-screen';
+
+// ----------------------------------------------------------------------
+
+export default function Loading() {
+  return <SplashScreen />;
+}
